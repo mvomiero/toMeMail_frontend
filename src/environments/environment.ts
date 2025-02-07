08b6a5dce176ccc2628860a49b3daa653cdeb1e6
Marco Vomiero <colocomarco1@gmail.com>
2025-02-07 12:02:58 +0100

    ignore environments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080/api' // ✅ Local dev API
+  //apiUrl: 'http://localhost:8080/api' // ✅ Local dev API
+  apiUrl: 'https://tome-mail-backend.azurewebsites.net/api'
 };
